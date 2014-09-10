896dd2ce1ea1b57dae735425158226b824115121
Hiroaki Nakamura <hnakamur@gmail.com>
2014-09-11 06:31:25 +0900

    Read stdout and stderr in real time with two goroutines

--- main.go
+++ main.go
@@ -5,22 +5,47 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+
+	"github.com/mgutz/ansi"
 )
 
 func main() {
 	cmd := exec.Command("./a.sh")
-	stdout, err := cmd.StdoutPipe()
+	err := runCommand(cmd)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func runCommand(cmd *exec.Cmd) error {
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+
 	if err := cmd.Start(); err != nil {
-		log.Fatal(err)
+		return err
 	}
-	scanner := bufio.NewScanner(stdout)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-	if err := cmd.Wait(); err != nil {
-		log.Fatal(err)
-	}
+
+	go func() {
+		stdoutHeader := ansi.Color("stdout:", "green")
+		stdoutScanner := bufio.NewScanner(stdout)
+		for stdoutScanner.Scan() {
+			fmt.Printf("%s%s\n", stdoutHeader, stdoutScanner.Text())
+		}
+	}()
+
+	go func() {
+		stderrHeader := ansi.Color("stderr:", "red")
+		stderrScanner := bufio.NewScanner(stderr)
+		for stderrScanner.Scan() {
+			fmt.Printf("%s%s\n", stderrHeader, stderrScanner.Text())
+		}
+	}()
+
+	return cmd.Wait()
 }
